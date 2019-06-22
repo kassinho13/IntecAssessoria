dc4ace82dca314350a9ae33503ededd8882f5e35
Cassio <cassio.menabue@gmail.com>
2019-06-22 09:31:10 -0300

    Implementacao do listener Donwload no web view e criacao do ToasCustom

--- app/src/main/java/br/com/intecassessoria/MainActivity.java
+++ app/src/main/java/br/com/intecassessoria/MainActivity.java
@@ -1,10 +1,23 @@
 package br.com.intecassessoria;
 
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 //https://www.androidpro.com.br/blog/desenvolvimento-android/webview-converter-site-aplicativo/
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
@@ -20,6 +33,62 @@ public class MainActivity extends AppCompatActivity {
         //Habilitando o JavaScript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        myWebView.setDownloadListener(new DownloadListener()
+        {
+
+            @Override
+
+
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLength) {
+
+                DownloadManager.Request request = new DownloadManager.Request(
+                        Uri.parse(url));
+
+
+                request.setMimeType(mimeType);
+
+
+                String cookies = CookieManager.getInstance().getCookie(url);
+
+
+                request.addRequestHeader("cookie", cookies);
+
+
+                request.addRequestHeader("User-Agent", userAgent);
+
+
+                request.setDescription("Baixando Arquivo");
+
+
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition,
+                        mimeType));
+
+
+                request.allowScanningByMediaScanner();
+
+
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(
+                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_custom,
+                        (ViewGroup) findViewById(R.id.custom_toast_container));
+
+                TextView text = layout.findViewById(R.id.text);
+                text.setText("Baixando Arquivo");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+
+            }});
     }
 
 
