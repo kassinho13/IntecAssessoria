570dfa0cead02483d7171109195c39c6eec70c3c
Cassio <cassio.menabue@gmail.com>
2019-06-30 07:59:16 -0300

    Implementacao do permissionRequest

--- app/src/main/java/br/com/intecassessoria/MainActivity.java
+++ app/src/main/java/br/com/intecassessoria/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.intecassessoria;
 
+import android.Manifest;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -21,6 +25,7 @@ import android.widget.Toast;
 //https://www.androidpro.com.br/blog/desenvolvimento-android/webview-converter-site-aplicativo/
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,62 +38,101 @@ public class MainActivity extends AppCompatActivity {
         //Habilitando o JavaScript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.setDownloadListener(new DownloadListener()
-        {
 
-            @Override
+        setActions();
 
+    }
+private void setActions(){
 
-            public void onDownloadStart(String url, String userAgent,
-                                        String contentDisposition, String mimeType,
-                                        long contentLength) {
+    myWebView.setDownloadListener(new DownloadListener()
+    {
 
-                DownloadManager.Request request = new DownloadManager.Request(
-                        Uri.parse(url));
+        @Override
 
 
-                request.setMimeType(mimeType);
+        public void onDownloadStart(String url, String userAgent,
+                                    String contentDisposition, String mimeType,
+                                    long contentLength) {
+
+            DownloadManager.Request request = new DownloadManager.Request(
+                    Uri.parse(url));
+            request.setMimeType(mimeType);
+            String cookies = CookieManager.getInstance().getCookie(url);
+            request.addRequestHeader("cookie", cookies);
+            request.addRequestHeader("User-Agent", userAgent);
+            request.setDescription("Arquivo Baixado com sucesso");
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition,mimeType));
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName( url, contentDisposition, mimeType));
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            try{
+                dm.enqueue(request);
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_custom,
+                        (ViewGroup) findViewById(R.id.custom_toast_container));
+
+                TextView text = layout.findViewById(R.id.text);
+                text.setText("Baixando Arquivo");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+            }catch (Exception e){
+                checkPermisionWrite();
 
 
-                String cookies = CookieManager.getInstance().getCookie(url);
+            }
 
 
-                request.addRequestHeader("cookie", cookies);
+        }});
+}
+    private void checkPermisionWrite() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,   Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
 
-                request.addRequestHeader("User-Agent", userAgent);
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
 
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            } else {
 
-                request.setDescription("Arquivo Baixado com sucesso");
+                // No explanation needed, we can request the permission.
 
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
 
-                request.setTitle(URLUtil.guessFileName(url, contentDisposition,
-                        mimeType));
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
 
-                request.allowScanningByMediaScanner();
+                } else {
 
-
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                request.setDestinationInExternalPublicDir(
-                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
-                                url, contentDisposition, mimeType));
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                try{
-                    dm.enqueue(request);
-                    LayoutInflater inflater = getLayoutInflater();
-                    View layout = inflater.inflate(R.layout.toast_custom,
-                            (ViewGroup) findViewById(R.id.custom_toast_container));
-
-                    TextView text = layout.findViewById(R.id.text);
-                    text.setText("Baixando Arquivo");
-
-                    Toast toast = new Toast(getApplicationContext());
-                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                    toast.setDuration(Toast.LENGTH_LONG);
-                    toast.setView(layout);
-                    toast.show();
-                }catch (Exception e){
                     LayoutInflater inflater = getLayoutInflater();
                     View layout = inflater.inflate(R.layout.toast_custom,
                             (ViewGroup) findViewById(R.id.custom_toast_container));
@@ -101,13 +145,12 @@ public class MainActivity extends AppCompatActivity {
                     toast.setDuration(Toast.LENGTH_LONG);
                     toast.setView(layout);
                     toast.show();
-
                 }
+                return;
+            }
 
-
-            }});
+        }
     }
-
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
